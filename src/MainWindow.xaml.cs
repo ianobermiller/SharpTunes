efda0375fb8289af641b7421deea96664a627009
Ian Obermiller <ian@obermillers.com>
2013-01-15 16:06:54 -0800

    Add a simple player class.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -7,9 +7,12 @@ namespace SharpTunes
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        Player player = new Player();
+
         public MainWindow()
         {
             InitializeComponent();
+            player.Load(@"E:\Dropbox\Music\Taylor Swift - Red\CD 1\08 - We Are Never Ever Getting Back Together.mp3").Play();
         }
     }
 }
---
+++ src/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using NAudio.Wave;
+
+namespace SharpTunes
+{
+    class Player
+    {
+        private WaveStream mainOutputStream = null;
+        private WaveOutEvent player = new WaveOutEvent();
+
+        public Player Load(string fileName)
+        {
+            if (this.mainOutputStream != null)
+            {
+                this.mainOutputStream.Close();
+            }
+
+            this.mainOutputStream = new Mp3FileReader(fileName);
+            WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream);
+            this.player.Init(volumeStream);
+            return this;
+        }
+
+        public Player Play()
+        {
+            this.player.Play();
+            return this;
+        }
+
+        public Player Pause()
+        {
+            this.player.Pause();
+            return this;
+        }
+
+        public Player Stop()
+        {
+            player.Pause();
+            this.mainOutputStream.Seek(0, SeekOrigin.Begin);
+            return this;
+        }
+    }
+}
